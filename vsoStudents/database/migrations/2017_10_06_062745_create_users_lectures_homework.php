07e6214e4e3973b3fc3440d8990d92789af64ce4
Milena Tomova <kokolina1888@gmail.com>
2017-10-06 09:45:35 +0300

    'DB'

---
+++ vsoStudents/database/migrations/2017_10_06_062745_create_users_lectures_homework.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersLecturesHomework extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_lectures_homework', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('lecture_id')->unsigned();
+            $table->foreign('lecture_id')->references('id')->on('lectures');
+            $table->foreign('user_id')->references('id')->on('users');            
+            $table->primary(array('user_id', 'lecture_id'));
+            $table->string('homework_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_lectures_homework');
+    }
+}
